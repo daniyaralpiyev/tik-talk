35952b1c46fec65b2dd8da40807ec0b0775bdca3
Daniyar <piranha0590@gmail.com>
2025-07-01 17:31:19 +0500

    added sidebar component

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {SvgIcon} from '../svg-icon/svg-icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [SvgIcon],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
+  menuItems = [
+    {
+      id: 1,
+      label: 'Моя Страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      id: 2,
+      label: 'Чаты',
+      icon: 'chat',
+      link: 'chat'
+    },
+    {
+      id: 3,
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    },
 
+  ]
 }
---
+++ src/app/common-ui/svg-icon/svg-icon.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]', // все svg у которых будут указываться атрибут icon станут этим компонентом
+  standalone: true,
+  imports: [],
+  template: '<svg><use [attr.href]="href"></use></svg>',
+  styles: ['']
+})
+export class SvgIcon {
+  @Input() icon = '';
+  get href() {
+    // через # обращаемся к нужному id-щнику иконки
+    return `/assets/svg/${this.icon}.svg#${this.icon}`;
+  }
+}

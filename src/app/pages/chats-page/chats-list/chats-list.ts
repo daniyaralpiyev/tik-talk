b65481fd68557ac420965ab2d5b426fa1fbe5bfb
Daniyar <piranha0590@gmail.com>
2025-08-01 01:06:59 +0500

    added part chats 41.00 minutes

--- src/app/pages/chats-page/chats-list/chats-list.ts
+++ src/app/pages/chats-page/chats-list/chats-list.ts
@@ -1,9 +1,11 @@
 import {Component, inject} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {ChatsBtn} from '../chats-btn/chats-btn';
 import {ChatsService} from '../../../data/services/chats.service';
 import {AsyncPipe} from '@angular/common';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {map, switchMap} from 'rxjs';
+import {startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chats-list',
@@ -20,5 +22,24 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 export class ChatsList {
   chatsService = inject(ChatsService);
 
-  chats$ = this.chatsService.getMyChats()
+  filterChatsControl = new FormControl();
+
+  // В поле Искать... при вводе ищет по имени и фамилий
+  chats$ = this.chatsService.getMyChats() // запрос на бэк и получили какое то кол-во чатов
+    .pipe(
+      switchMap(chats => { // изменили стрим
+        return this.filterChatsControl.valueChanges // подписались на изменение контрола
+          .pipe(
+            startWith(''),
+            // теперь когда контрол меняется можем оперировать chats и значением который дал контрол в inputValue
+            map(inputValue => {
+              // меняем итоговое значение стрима на отфильтрованные чаты
+              return chats.filter(chat => {
+                return `${chat.userFrom.firstName} ${chat.userFrom.lastName}`
+                  .toLowerCase().includes(inputValue.toLowerCase());
+              })
+            })
+          )
+      })
+    )
 }

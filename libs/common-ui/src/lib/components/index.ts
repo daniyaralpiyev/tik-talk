ae9658802064d6300f714d947ac7777affbbf714
Daniyar <piranha0590@gmail.com>
2025-10-22 17:56:11 +0500

    fix forms 7

--- libs/common-ui/src/lib/components/index.ts
+++ libs/common-ui/src/lib/components/index.ts
@@ -1,6 +1,5 @@
 import { AvatarCircle } from "./avatar-circle/avatar-circle";
 import { InfiniteScrollTrigger } from './infinite-scroll-trigger/infinite-scroll-trigger.component';
-import { PasswordInput } from './password-input/password-input';
 import { SvgIcon } from './svg-icon/svg-icon';
 import { TtInput } from './tt-input/tt-input';
 
@@ -8,6 +7,5 @@ export {
   AvatarCircle,
   SvgIcon,
 	InfiniteScrollTrigger,
-	TtInput,
-	PasswordInput
+	TtInput
 }
